f3992f91d8a3c03639d5852c639257e8aa5e68d2
Emre <ozturkemre@mail.com>
2023-04-18 22:38:10 +0300

    S5G1 tamamlandı

--- src/index.js
+++ src/index.js
@@ -43,3 +43,54 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+
+const menuItems = document.querySelectorAll("nav a");
+menuItems.forEach((item,index) => {
+  item.textContent = siteContent.nav[`nav-item-${index + 1}`]
+  item.setAttribute("class","italic")
+})
+
+const picLogo = document.querySelector("#logo-img");
+picLogo.src = siteContent.images["logo-img"];
+
+const ctaText = document.querySelector(".cta-text h1");
+ctaText.textContent = siteContent.cta.h1;
+
+const btn = document.querySelector(".cta-text button");
+btn.textContent = siteContent.cta.button;
+
+const picCta = document.querySelector("#cta-img");
+picCta.src = siteContent.images["cta-img"];
+
+const h4TopContent = document.querySelectorAll(".top-content h4");
+h4TopContent[0].textContent = siteContent["ana-içerik"]["özellikler-h4"];
+h4TopContent[1].textContent = siteContent["ana-içerik"]["hakkımızda-h4"];
+
+const pTopContent = document.querySelectorAll(".top-content p");
+pTopContent[0].textContent = siteContent["ana-içerik"]["özellikler-içerik"];
+pTopContent[1].textContent = siteContent["ana-içerik"]["hakkımızda-içerik"];
+
+const picMiddle = document.querySelector("#middle-img");
+picMiddle.src = siteContent.images["accent-img"];
+
+const h4BottomContent = document.querySelectorAll(".bottom-content h4");
+h4BottomContent[0].textContent = siteContent["ana-içerik"]["servisler-h4"];
+h4BottomContent[1].textContent = siteContent["ana-içerik"]["ürünler-h4"];
+h4BottomContent[2].textContent = siteContent["ana-içerik"]["vizyon-h4"];
+
+const pBottomContent = document.querySelectorAll(".bottom-content p");
+pBottomContent[0].textContent = siteContent["ana-içerik"]["servisler-içeriği"];
+pBottomContent[1].textContent = siteContent["ana-içerik"]["ürünler-içeriği"];
+pBottomContent[2].textContent = siteContent["ana-içerik"]["vizyon-içeriği"];
+
+const iletisim = document.querySelectorAll(".contact h4");
+iletisim[0].textContent = siteContent["iletisim"]["iletişim-h4"];
+
+const pContact = document.querySelectorAll(".contact p");
+pContact[0].textContent = siteContent["iletisim"]["adres"];
+pContact[1].textContent = siteContent["iletisim"]["telefon"];
+pContact[2].textContent = siteContent["iletisim"]["email"];
+
+const foot = document.querySelector("footer a");
+foot.textContent = siteContent["footer"]["copyright"];
+foot.classList.add("bold");
